ba7181f31bd32e4e2cfecf62fe68c5a6e9cadc1b
codeTIT4N <lokeshkumar636@yahoo.com>
2022-10-18 12:51:43 +0530

    update 2

---
+++ 2-core-types/app.js
@@ -0,0 +1,12 @@
+function add(n1, n2) {
+    return n1 + n2;
+}
+var number1 = 5;
+var number2 = 2.8;
+var person = {
+    name: 'Lokesh',
+    age: 22
+};
+var result = add(number1, number2);
+console.log(result);
+// console.log(person.nickname);
---
+++ 2-core-types/app.ts
@@ -0,0 +1,17 @@
+function add(n1: number, n2: number) {
+  return n1 + n2;
+}
+
+const number1 = 5;
+const number2 = 2.8;
+const person: {
+  name: string;
+  age: number;
+} = {
+  name: 'Lokesh',
+  age: 22
+}
+
+const result = add(number1, number2);
+console.log(result);
+// console.log(person.nickname);
