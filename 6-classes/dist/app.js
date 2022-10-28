7632a77e92a7944a718c81db45aa8014389bebcc
codeTIT4N <lokeshkumar636@yahoo.com>
2022-10-28 13:34:06 +0530

    update 9

--- 6-classes/dist/app.js
+++ 6-classes/dist/app.js
@@ -1,9 +1,33 @@
 "use strict";
 class Department {
-    // for initializing variables inside of the class
-    constructor(n) {
-        this.name = n;
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+        this.employees = [];
+    }
+    describe() {
+        console.log('Department: ' + this.name);
+    }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInfo() {
+        console.log(this.employees.length);
+        console.log(this.employees);
     }
 }
-const it = new Department('IT'); //creating new object
-console.log(it);
+class AccountingDepartment extends Department {
+    constructor(id, admins) {
+        super(id, 'Accounting');
+        this.admins = admins;
+        this.admins = admins;
+    }
+    addEmployee(name) {
+        if (name === 'Aniket') {
+            return;
+        }
+        this.employees.push(name);
+    }
+}
+const accounting = new AccountingDepartment('d2', ['Krishna', 'Vikas']);
+console.log(accounting);
--- 6-classes/src/app.ts
+++ 6-classes/src/app.ts
@@ -1,11 +1,55 @@
 class Department {
-    name: string;
-    // for initializing variables inside of the class
-    constructor(n: string) {
-        this.name = n;
+    // private readonly id: string
+    // public readonly name: string;
+    protected employees: string[] = [];
+
+    constructor(private readonly id: string, public name: string) {
+    }
+
+    describe(this: Department) {
+        console.log('Department: ' + this.name);
+    }
+
+    addEmployee(employee: string) {
+        this.employees.push(employee);
+    }
+
+    printEmployeeInfo() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+
     }
 }
 
-const it = new Department('IT'); //creating new object
 
-console.log(it);
+// Inheritance
+class AccountingDepartment extends Department {
+
+    constructor(id: string, public admins: string[]) {
+        super(id, 'Accounting')
+        this.admins = admins;
+    }
+
+    addEmployee(name: string) {
+        if (name === 'Aniket') {
+            return;
+        }
+        this.employees.push(name)
+    }
+}
+
+const accounting = new AccountingDepartment('d2', ['Krishna', 'Vikas'])
+console.log(accounting);
+
+
+// const it = new Department('d1', 'IT'); //creating new object
+// it.addEmployee('ankit');
+// it.addEmployee('bhuvan');
+// it.addEmployee('vedika');
+
+// it.describe();
+// it.printEmployeeInfo();
+
+// const itCopy = { name: 'DUMMY', describe: it.describe };
+
+// itCopy.describe()
