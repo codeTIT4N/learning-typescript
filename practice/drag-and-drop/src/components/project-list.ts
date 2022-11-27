47d5337af1c74d8ce54ee6faf38b0d2b4a25ad52
codeTIT4N <lokeshkumar636@yahoo.com>
2022-11-27 23:13:14 +0530

    update 32

--- practice/drag-and-drop/src/components/project-list.ts
+++ practice/drag-and-drop/src/components/project-list.ts
@@ -1,9 +1,9 @@
-import Component from "./base-component.js";
-import { Project, ProjectStatus } from "../models/project.js";
-import { Autobind } from "../decorators/autobind.js";
-import { projectState } from "../state/project-state.js";
-import { ProjectItem } from "./project-item.js";
-import { DragTarget } from "../models/drag-drop.js";
+import Component from "./base-component";
+import { Project, ProjectStatus } from "../models/project";
+import { Autobind } from "../decorators/autobind";
+import { projectState } from "../state/project-state";
+import { ProjectItem } from "./project-item";
+import { DragTarget } from "../models/drag-drop";
 // ProjectList Class
 export class ProjectList extends Component<HTMLDivElement, HTMLElement> implements DragTarget {
 
